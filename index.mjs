fad4914855336950a351848f34dbbd942fb1ec0d
daliborgogic <mail@daliborgogic.com>
2019-08-25 18:25:40 +0200

    minor

--- index.mjs
+++ index.mjs
@@ -1,23 +1,17 @@
+import express from 'express'
 import { parseRequest } from './lib/parser.mjs'
 import { getScreenshot } from './lib/chromium.mjs'
 import { getHtml } from './lib/template.mjs'
 import { writeTempFile, pathToFileURL } from './lib/file.mjs'
 
-const isHtmlDebug = process.env.OG_HTML_DEBUG === '1'
-import express from 'express'
 const app = express()
 
 async function handler(req, res) {
   try {
     const parsedReq = parseRequest(req)
     const html = getHtml(parsedReq)
-    if (isHtmlDebug) {
-        res.setHeader('Content-Type', 'text/html')
-        res.end(html)
-        return
-    }
     const { text, fileType } = parsedReq
-    const filePath = await writeTempFile(text, html);
+    const filePath = await writeTempFile(text, html)
     const fileUrl = pathToFileURL(filePath);
     const file = await getScreenshot(fileUrl, fileType)
     res.statusCode = 200;
@@ -25,17 +19,16 @@ async function handler(req, res) {
     res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`)
     res.end(file)
   } catch (error) {
-      res.statusCode = 500
-      res.setHeader('Content-Type', 'text/html')
-      res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>')
-      console.error(error)
+    res.statusCode = 500
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>')
+    console.error(error)
   }
 }
 
-app.get('*', async(req, res) => {
-  const x = await handler(req, res)
-  res.status(200).send(x)
-})
+app.get('/', async(req, res) =>
+  res.status(200).send(await handler(req, res))
+)
 
 app.listen(3000)
 
